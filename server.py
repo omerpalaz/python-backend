0286d15ec016e3ff6361a36573c4cf4b40ca860d
omerpalaz <omer.palaz@gmail.com>
2020-09-13 12:52:24 -0700

    Created server.py

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+# /user-profile/fatih
+# /user-profile/omer
+@app.route('/user-profile/<name>')
+def my_name(name):
+    return 'My profile name: ' + name
+
+@app.route('/signIn')
+def sign_in():
+    email_val = request.args.get('email')
+    password_val = request.args.get('password')
+    if not (email_val and password_val):
+        return {
+            'error': 'email and password do not exist!!!'
+        }
+    return {
+        'email': email_val,
+        'password': password_val,
+    }
+
+if __name__ == '__main__':
+   app.run()
